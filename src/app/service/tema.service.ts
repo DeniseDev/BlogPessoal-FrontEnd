5f0cd1f3946d0a824166e241c5da4ba2102b4321
DeniseDev <Denise.liiima@outlook.com>
2022-06-07 16:12:15 -0300

    Implementação de put e delete de temas

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,7 +19,20 @@ export class TemaService {
     return this.http.get<Tema[]>('https://blogdenise.herokuapp.com/temas', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`https://blogdenise.herokuapp.com/temas/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blogdenise.herokuapp.com/temas', tema, this.token)
   }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogdenise.herokuapp.com/temas', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`https://blogdenise.herokuapp.com/temas/${id}` , this.token)
+  }
+
 }
