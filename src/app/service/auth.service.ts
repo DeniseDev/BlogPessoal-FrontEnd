d99bd5fa4c19ea1f33837c74c368d5fb6b16bb53
DeniseDev <Denise.liiima@outlook.com>
2022-06-10 10:38:27 -0300

    Implementação do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -10,9 +10,20 @@ import { UserLogin } from '../model/UserLogin';
 })
 export class AuthService {
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor( private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization',environment.token),
+    };
+    refreshToken(){
+      this.token = {
+        headers: new HttpHeaders().set('Authorization', environment.token),
+      };
+    }
+
+    getByIdUser(id: number): Observable<User>{
+      return this.http.get<User>(`https://blogdenise.herokuapp.com/usuarios/${id}`, this.token)
+    }
 
    entrar(UserLogin: UserLogin ): Observable<UserLogin> {
     return this.http.post<UserLogin>('https://blogdenise.herokuapp.com/usuarios/logar', UserLogin)
